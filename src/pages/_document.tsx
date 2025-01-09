63d5434e8fdee33e75d9c90b91ecf00031c74edc
juanp-ctrl <juanpa11121@hotmail.com>
2025-01-09 10:39:32 -0500

    Add animated (when supported) favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="theme-color" content="#000000" />
+        <link rel="icon" type="image/gif" href="/favicon.gif" />
       </Head>
       <body className="antialiased">
         <Main />
